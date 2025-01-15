02f707c53341d8e660a623ab71d032546cc99a4d
Alessandro <95830927+al-e-dev@users.noreply.github.com>
2025-01-15 17:57:33 -0500

    Update examples.js

--- src/routers/examples.js
+++ src/routers/examples.js
@@ -3,7 +3,7 @@ exports.routes = {
    path: '/api',
    method: 'get',
    execution: async (req, res) => {
-      res.json({ json: "api is online", owner: "alexito" })
+      res.json({ json: "api is online", owner: "alexito", powered: "Nazi" })
    },
    error: false
 }
