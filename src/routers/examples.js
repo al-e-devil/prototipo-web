a953eefe63b877c2871c4007dec9f5578f11ec92
al-e-devil <aledevil@outlook.es>
2025-04-22 03:53:24 +0000

    update

--- src/routers/examples.js
+++
@@ -1,9 +0,0 @@
-exports.routes = {
-   category: 'main',
-   path: '/api',
-   method: 'get',
-   execution: async (req, res) => {
-      res.json({ json: "api is online", owner: "alexito", powered: "Nazi d" })
-   },
-   error: false
-}
--- src/routers/welcome.js
+++
@@ -1,9 +0,0 @@
-exports.routes = {
-   category: 'main',
-   path: '/',
-   method: 'get',
-   execution: async (req, res) => {
-      res.status(200).render('welcome')
-   },
-   error: false
-}
